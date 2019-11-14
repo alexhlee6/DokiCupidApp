47fb4209a3c8cd82652b620e0d483d5df220cbc4
alex629lee <alex629lee@yahoo.com>
2019-11-14 15:38:36 -0800

    Restarting and reseeding Heroku database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,19 @@
 
 
 user1 = User.create(username: "demo_user", password: "demo_user")
-file1 = File.open('app/assets/images/default_boy.jpg')
-user1.photo.attach(io: file1, filename: "default_boy.jpg")
+file1A = File.open('app/assets/images/default_boy.jpg')
+user1.photo.attach(io: file1A, filename: "default_boy.jpg")
 profile1 = Profile.new(user_id: "", fname: "DemoUser", zipcode: 94102, bio: "This is the profile for a demo user!", identify_as: "Gender Neutral", looking_for: "True Love", compatibility_answers: "NNYNNYYY")
 profile1.user_id = user1.id
 profile1.save
-profile1.photos.attach(io: file1, filename: "default_boy.jpg")
+file1B = File.open('app/assets/images/default_boy.jpg')
+profile1.photos.attach(io: file1B, filename: "default_boy.jpg")
 
 user2 = User.create(username: "alexlee", password: "alexlee")
-file2 = File.open('app/assets/images/akko.jpg')
-user2.photo.attach(io: file2, filename: "akko.jpg")
+file2A = File.open('app/assets/images/akko.jpg')
+user2.photo.attach(io: file2A, filename: "akko.jpg")
 profile2 = Profile.new(user_id: "", fname: "Alex", zipcode: 92122, bio: "Hi I'm Alex, the creator of this app!", identify_as: "Female", looking_for: "Friends", compatibility_answers: "YNNYYNYY")
 profile2.user_id = user2.id
 profile2.save
-profile2.photos.attach(io: file2, filename: "akko.jpg")
+file2B = File.open('app/assets/images/akko.jpg')
+profile2.photos.attach(io: file2B, filename: "akko.jpg")
