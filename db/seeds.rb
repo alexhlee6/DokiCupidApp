45d2e6215832a7ac7d05bdd37772e8c8d7951642
alex629lee <alex629lee@yahoo.com>
2019-11-14 14:49:12 -0800

    Attempt to seed Heroku db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+user1 = User.create(username: "demo_user", password: "demo_user")
+file1 = File.open('app/assets/images/default_boy.jpg')
+user1.photo.attach(io: file1, filename: "default_boy.jpg")
+
+
+user2 = User.create(username: "alexlee", password: "alexlee")
+file2 = File.open('app/assets/images/akko.jpg')
+user2.photo.attach(io: file2, filename: "akko.jpg")
+
